779b595330f2c25f5be7fa0a67fb22bd7d336fa7
gns0304 <gns0304@naver.com>
2022-11-13 17:52:03 +0900

    feat: change chrome options for ubuntu cli

--- lib/finance.py
+++ lib/finance.py
@@ -14,8 +14,8 @@ class Investing:
         try:
             options = webdriver.ChromeOptions()
             options.add_argument('headless')
-            options.add_argument('window-size=1920x1080')
-            options.add_argument("disable-gpu")
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
             driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
             wait = WebDriverWait(driver, 5)
