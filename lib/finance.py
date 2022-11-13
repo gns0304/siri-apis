f80a834c0f25b43c96ad540a481ebe73778bb6b7
gns0304 <gns0304@naver.com>
2022-11-13 18:27:15 +0900

    feat: yahoo finance

--- lib/finance.py
+++ lib/finance.py
@@ -6,50 +6,47 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 
+
 class Yahoo:
 
     @staticmethod
     def get_finance_data(code):
-        # try:
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        try:
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-        wait = WebDriverWait(driver, 5)
+            wait = WebDriverWait(driver, 5)
 
-        driver.get("https://finance.yahoo.com/quote/{}".format(code))
-        wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="quote-header-info"]/div[3]/div[1]/div/fin-streamer[1]')))
+            driver.get("https://finance.yahoo.com/quote/{}".format(code))
+            wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="quote-header-info"]/div[3]/div[1]/div/fin-streamer[1]')))
 
-        title = driver.find_element(By.XPATH, '//*[@id="quote-header-info"]/div[2]/div[1]/div[1]/h1').text
-        quote = driver.find_element(By.XPATH, '//*[@id="quote-header-info"]/div[3]/div[1]/div/fin-streamer[1]').text
-        point = driver.find_element(By.XPATH, '//*[@id="quote-header-info"]/div[3]/div[1]/div/fin-streamer[2]').text
-        percent = driver.find_element(By.XPATH, '//*[@id="quote-header-info"]/div[3]/div[1]/div/fin-streamer[3]').text
+            title = driver.find_element(By.XPATH, '//*[@id="quote-header-info"]/div[2]/div[1]/div[1]/h1').text
+            quote = driver.find_element(By.XPATH, '//*[@id="quote-header-info"]/div[3]/div[1]/div/fin-streamer[1]').text
+            point = driver.find_element(By.XPATH, '//*[@id="quote-header-info"]/div[3]/div[1]/div/fin-streamer[2]').text
+            percent = driver.find_element(By.XPATH, '//*[@id="quote-header-info"]/div[3]/div[1]/div/fin-streamer[3]').text
 
-        quote = float(re.sub(r'[^0-9.]', "", quote))
-        point = float(point)
-        percent = float(re.sub(r'[^0-9.+-]', '', percent))
+            quote = float(re.sub(r'[^0-9.]', "", quote))
+            point = float(point)
+            percent = float(re.sub(r'[^0-9.+-]', '', percent))
 
-        driver.close()
+            driver.close()
 
-        return {
-            "state": "Success",
-            "data": {
-                "instrument-name": title,
-                "instrument-price-last": quote,
-                "instrument-price_change-value": point,
-                "instrument-price-change-percent": percent
+            return {
+                "state": "Success",
+                "data": {
+                    "instrument-name": title,
+                    "instrument-price-last": quote,
+                    "instrument-price_change-value": point,
+                    "instrument-price-change-percent": percent
+                }
             }
-        }
 
-        # except Exception as e:
-        #
-        #     return {
-        #         "state": "Failed",
-        #         "data": e
-        #     }
+        except Exception as e:
 
-
-if __name__ == '__main__':
-    print(Yahoo.get_finance_data("^IXIC"))
+            return {
+                "state": "Failed",
+                "data": e
+            }
